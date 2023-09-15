8bc18e5f8af2d98c5ebd5b017527d25c2f133b68
KaranjaK <karanjakelvin77@gmail.com>
2023-09-15 10:06:00 +0300

    Revert "📦 Creates the educationsection component" This commit has been made to the wrong branch This reverts commit bb47b757c2e62bd393be9416c3884916f53a5899.

--- libs/features/pages/home/src/lib/components/education-section/education-section.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-website-education-section',
-  templateUrl: './education-section.component.html',
-  styleUrls: ['./education-section.component.scss'],
-})
-export class EducationSectionComponent {}
